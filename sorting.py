2a86a8f2b56d372c6bb5f5c8a5551a91e69f2eec
danwilliams34 <daniel.williams1@ntlworld.com>
2014-05-08 09:54:21 +0100

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -4,13 +4,3 @@ def swap(alist, index):
     alist[index] = b
     alist[index+1] = a
     return (alist)
-
-def bsort(alist):
-    swaps = True
-    while swaps:
-        swaps = False
-        for i in range(len(alist)-1):
-            if (alist[i] > alist[i+1]):
-                alist = swap(alist, i)
-                swaps = True
-    return (alist)
