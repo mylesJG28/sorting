360ea3bb4f1b169cd6b7abfb741fb0db3547eeef
danwilliams34 <daniel.williams1@ntlworld.com>
2014-05-08 09:55:10 +0100

    Update sorttests.py

--- sorttests.py
+++ sorttests.py
@@ -1,4 +1,4 @@
-import sorting
+import activity3
 import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -8,13 +8,6 @@ class TestSequenceFunctions(unittest.TestCase):
         inlist = [4,3,2,1]
         outlist = [4,2,3,1]
         assert (sorting.swap(inlist,1)==outlist) #test 1 fails, swap does not work as expected.
-
-    def test2(self):
-        '''Test 2 checks that the bubble sort can sort a list of 4 items reversed.
-        '''
-        inlist = [4,3,2,1]
-        outlist = [1,2,3,4]
-        assert (sorting.bsort(inlist)==outlist) #test 2 fails - list not sorted by bubble sort
 
 if __name__ == '__main__':
     unittest.main()
